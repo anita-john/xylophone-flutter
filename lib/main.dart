c0235a25b4f1a981e04ba7f2cb6d33071030ce91
Anita John <anitaj1@learn.cssd.ab.ca>
2020-05-08 12:25:02 -0600

    Imported Dart Packages for audio

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
 
 void main() => runApp(XylophoneApp());
 
@@ -8,7 +10,12 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Container(child: FlatButton( onPressed: (){
+
+            final player =AudioCache();
+            player.play('note1.wav');
+
+          }, child: Text('Click Me!'))),
         ),
       ),
     );
